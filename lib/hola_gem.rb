599af992e16a4c01fe577274e4466b8a446db86f
wkbang <wkbang@gmail.com>
2014-08-29 15:14:19 -0400

    initial check-in

---
+++ lib/hola_gem.rb
@@ -0,0 +1,8 @@
+require "hola_gem/version"
+
+module HolaGem
+  # Your code goes here...
+  def self.msg
+    puts "wkbang's first ruby gem!!!"
+  end
+end
---
+++ lib/hola_gem/version.rb
@@ -0,0 +1,3 @@
+module HolaGem
+  VERSION = "0.0.1"
+end

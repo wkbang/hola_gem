b52e58538fea1e8b2be9f960817fc0839262b45b
wkbang <wkbang@gmail.com>
2014-09-03 11:56:08 -0400

    refactor to ruby style

--- lib/hola_gem.rb
+++ lib/hola_gem.rb
@@ -3,6 +3,6 @@ require "hola_gem/version"
 module HolaGem
   # Your code goes here...
   def self.msg
-    puts "wkbang's first ruby gem!!!"
+    puts "wkbang's first ruby gem!!! -  branch1"
   end
 end
